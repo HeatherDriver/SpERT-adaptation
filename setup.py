0eccbd6feb24cc50bc7a0ab91068da006cf83c15
tongplw <bangkok.tong@gmail.com>
2020-11-03 23:45:33 +0700

    changed conf type

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='spert',
     packages=find_packages(),
-    data_files=[('data', ['*.json', '*.conf'])],
-    package_data={'config': []},
+    package_data={'json': ['*.json'],
+                  'config': ['*.cfg']},
     include_package_data=True,
     version='1.0.0',
     description='spert',
