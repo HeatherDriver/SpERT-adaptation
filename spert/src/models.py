b29daebc8892e1d42231c20e38117363eef556fd
tongplw <bangkok.tong@gmail.com>
2020-11-03 17:41:23 +0700

    fixed a little import syntax bug

--- spert/src/models.py
+++ spert/src/models.py
@@ -4,8 +4,8 @@ from transformers import BertConfig
 from transformers import BertModel
 from transformers import BertPreTrainedModel
 
-from . import .sampling
-from . import .util
+from . import sampling
+from . import util
 
 
 def get_token(h: torch.tensor, x: torch.tensor, token: int):
